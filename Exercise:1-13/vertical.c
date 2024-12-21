d0d4254a20bd5016f18f9647eaf90b0f72841e21
githyan <nantadryanek4@gmail.com>
2024-12-22 03:03:53 +0700

    solved vertical version

---
+++ Exercise:1-13/vertical.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 26
+#define IN 1
+#define OUT 0
+
+int main(){
+    int gChar, nl, nc, nw;
+    char save_words[MAX];
+    int state = OUT;
+
+    for (int i = 0; i < MAX; i++) {
+        save_words[i] = 0;
+    }
+    nl = nc = nw = 0;
+    while ((gChar = getchar()) != EOF) {
+        nc++;
+        if (gChar == '\n')
+            nl++;
+        if (gChar == ' ' || gChar == '\n' || gChar == '\t') {
+            state = OUT;
+        }else {
+            state = IN;
+            nw++;
+            for(int i = 0; save_words[i] == OUT; i++){
+                save_words[nc] = gChar;
+                state = OUT;
+            }
+        }
+    }
+
+    char copy_words[MAX][MAX] = {'\0'};
+    int sep_word = 0;
+
+    for (int i = 0; i < nc; i++){
+        if (save_words[i] == ' ' || save_words[i] == '\0') {
+            copy_words[state][sep_word] = '\0';
+            state++;
+            sep_word = 0;
+        } else {
+            copy_words[state][sep_word] = save_words[i];
+            sep_word++;
+        }
+    }
+
+    int len[state];
+    printf("lengths of words :\n");
+    for (int i = 1; i <= state; i++) {
+        len[i] = 0;
+        if (len[i] == 0){
+            len[i] = strlen(copy_words[i]);
+        }
+    }
+
+    for(int i = nc; i >= 1; --i){
+        for(int j = 1; j <= state; ++j){
+            if(i <= len[j])
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+
+    return OUT;
+}
+
