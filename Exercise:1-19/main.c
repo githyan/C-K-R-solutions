70d6a8047924430db53f5647c87cbb2fdbad9eb5
githyan <nantadryanek4@gmail.com>
2024-12-24 11:54:49 +0700

    solved problem Exercise 1-19

---
+++ Exercise:1-19/main.c
@@ -0,0 +1,44 @@
+#include <linux/limits.h>
+#include <stdio.h>
+
+#define MAXLEN 50
+
+int get_line(char src[], int lim);
+void reverse(char from[], char to[]);
+int main(){
+    int len, max = 0;
+    char line[MAXLEN] = {'\0'};
+    char dest[MAXLEN] = {'\0'};
+
+    while ((len = get_line(line, MAXLEN)) > 0) {
+        if(len > max){
+            max = len;
+            reverse(dest, line);
+        }
+    }
+    if(max > 0)
+        for (int j = MAXLEN; j >= 0; j--) {
+            printf("%c", dest[j]);       
+        }
+
+    return 0;
+}
+
+int get_line(char src[], int lim) {
+    int c, i = 0;
+
+    for(; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+        src[i] = c;
+    }
+    if(c == '\n'){
+        src[i] = c;
+        i++;
+    }
+    src[i] = '\0';
+   
+    return i;
+}
+
+void reverse(char src[], char to[]) {
+    for (int i = 0; (src[i] = to[i]) != '\0'; i++);
+}
